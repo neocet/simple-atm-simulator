cfbe6115887d4d639803191c532f865451c287ff
Nitiya05 <104195287+Nitiya05@users.noreply.github.com>
2022-05-16 11:49:37 +0700

    Update pola_segitiga.py

--- python/pola_segitiga.py
+++ python/pola_segitiga.py
@@ -1,1 +1,16 @@
 # Code goes here
+print('\nsegitiga sama kaki\n')
+a = 6
+s = a - 1 # for space
+def new_func():
+    print(' \n ')
+
+for i in range(0, a):
+    for j in range(0, s):
+        print(' ', end='')
+    s -= 1
+    for j in range(0, i + 1):
+        print('* ', end='')
+ 
+    print(' ')
+print('\n')
