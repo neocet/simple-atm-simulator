c7efd6d2e6f59ec02eba35ed1f5f243bd521de6b
Teuku Beuraja Laksamana <ne0cet01@gmail.com>
2022-05-20 15:25:39 +0700

    Update data_sorting.py
    Removed excessive spacing

--- python/data_sorting.py
+++ python/data_sorting.py
@@ -2,10 +2,10 @@
 
 #inisialisasi variabel angka
 daftarAngka = [11,4,5,2,5,6,93,29]
-print ('Daftar Angka sebelum di sorting: ',daftarAngka)
+print ('Daftar Angka sebelum di sorting:', daftarAngka)
 
 #mengurutkan data angka dari terkecil ke terbesar
 daftarAngka.sort ()
 
 #tampilan nilai dari variabel daftar angka setelah pengurutan
-print ('Daftar Angka setelah di sorting: ',daftarAngka)
+print ('Daftar Angka setelah di sorting:', daftarAngka)
